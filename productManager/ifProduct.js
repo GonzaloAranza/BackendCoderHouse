6a7b5ac607671789c226ea4d28b98bea4d6fc99f
GonzaloAranza <g.f.aranzamendi@gmail.com>
2023-03-21 18:28:47 -0300

    productManagerWithFiles

--- productManager/ifProduct.js
+++ productManager/ifProduct.js
@@ -1,4 +1,8 @@
-let obj = {name:'juan'}
+let valoresOriginales = [1,2,3,4,5]
 
-if(!undefined)
-    console.log('hola')
+function miFuncion(v1,v2,v3){
+    return v1*v2
+}
+
+const resMiFuncion = valoresOriginales.filter(miFuncion)
+console.log(resMiFuncion)
