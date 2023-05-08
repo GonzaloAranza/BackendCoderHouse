b1cba8ba066975aa03212833d175e0f7dbbe3598
GonzaloAranza <g.f.aranzamendi@gmail.com>
2023-05-08 20:31:25 -0300

    cdeploy

---
+++ segundaPreEntrega/src/controllers/product.js
@@ -0,0 +1,69 @@
+import { request, response } from "express";
+import ProductManager from "../managers/productManager.js";
+import { productExist } from "../helpers/dbValidators.js";
+
+
+export const getProducts = async (req = request, res = response) => {
+    try {
+        const paginate = req.query;
+        const manager = new ProductManager();
+        const result = await manager.getAll(paginate);
+        res.send({ status: "sucess", ...result });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+};
+
+export const getProductById = async (req = request, res = response) => {
+    try {
+        const { pid } = req.params;
+        await productExist(pid);
+        const manager = new ProductManager();
+        const result = await manager.getOne(pid);
+        res.send(result);
+    } catch (error) {
+        res.status(404).send({ error: error.message });
+    }
+};
+
+export const postProduct = async (req = request, res = response) => {
+    try {
+        const { body } = req;
+        const manager = new ProductManager();
+        const result = await manager.create(body);
+        res.status(201).send({ msg: "Producto creado", result });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+};
+
+export const putProduct = async (req = request, res = response) => {
+    try {
+        const { pid } = req.params;
+        const { id, ...rest } = req.body;
+        const manager = new ProductManager();
+        const codeExist = await manager.getOneCode(rest.code);
+        if (codeExist) {
+            if (codeExist?.id.toString() !== pid) throw new Error("El codigo ingresado ya esta siendo utilizado por otro producto");
+        };
+        await productExist(pid);
+
+        const result = await manager.update(pid, rest);
+
+        res.send({ msg: "Producto actualizado", result });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+};
+
+export const deleteProduct = async (req = request, res = response) => {
+    try {
+        const { pid } = req.params;
+        const manager = new ProductManager();
+        const result = await manager.delete(pid);
+
+        res.send({ msg: "Producto eliminado", result });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+}
