c7b545a3e12ba3c9ab29cddf08cd3d3b1745b458
GonzaloAranza <g.f.aranzamendi@gmail.com>
2023-05-09 00:01:29 -0300

    changes

--- segundaPreEntrega/src/app.js
+++ segundaPreEntrega/src/app.js
@@ -2,7 +2,9 @@ import * as dotenv from 'dotenv';
 import Server from './models/server.js';
 
 const { NODE_ENV } = process.env;
+
 const env = NODE_ENV === 'test' ? '.env.test' : '.env';
+
 dotenv.config({ path: env });
 
 const server = new Server();
--- segundaPreEntrega/src/routes/productsRouter.js
+++ segundaPreEntrega/src/routes/productsRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { check } from "express-validator";
 import inputsValidation from "../middlewares/inputsValidate.js";
 import { codeExist, productExist } from "../helpers/dbValidators.js";
-import { getProducts, getProductById, postProduct, putProduct, deleteProduct } from "../controller/product.js";
+import { getProducts, getProductById, postProduct, putProduct, deleteProduct } from "../controllers/product.js";
 
 const router = Router();
 
