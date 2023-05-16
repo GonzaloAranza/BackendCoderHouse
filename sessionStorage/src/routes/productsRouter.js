0351608069fb94b4163284ec28848c347a91efdb
GonzaloAranza <g.f.aranzamendi@gmail.com>
2023-05-16 14:08:56 -0300

    deploy entrega

---
+++ sessionStorage/src/routes/productsRouter.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import inputsValidation from "../middlewares/inputsValidate.js";
+import { codeExist, productExist } from "../helpers/dbValidators.js";
+import { getProducts, getProductById, postProduct, putProduct, deleteProduct } from "../controllers/product.js";
+
+const router = Router();
+
+router.get('/', getProducts);
+
+router.get('/:pid', [
+    check('pid', 'No es un id valido').isMongoId(),
+    check('pid').custom(productExist).escape(),
+    inputsValidation
+], getProductById);
+
+router.post('/', [
+    check('title', 'El titulo es obligatorio').trim().notEmpty().escape(),
+    check('description', 'La descripcion es obligatorio').trim().notEmpty().escape(),
+    check('code', 'El codigo es obligatorio').trim().notEmpty().custom(codeExist).escape(),
+    check('stock', 'El stock es obligatorio').trim().escape(),
+    check('category', 'La categoria es obligatorio').trim().notEmpty().escape(),
+    inputsValidation
+], postProduct);
+
+router.put('/:pid', [
+    check('pid', 'No es un id valido').isMongoId(),
+    check('pid').custom(productExist).escape(),
+    check('title', 'El titulo es obligatorio').trim().notEmpty().escape(),
+    check('description', 'La descripcion es obligatorio').trim().notEmpty().escape(),
+    check('code', 'El codigo es obligatorio').notEmpty().escape(),
+    check('stock', 'El stock es obligatorio').notEmpty().escape(),
+    check('category', 'La categoria es obligatorio').trim().notEmpty().escape(),
+    inputsValidation
+], putProduct);
+
+router.delete('/:pid', [
+    check('pid', 'No es un id valido').isMongoId(),
+    check('pid').custom(productExist).escape(),
+    inputsValidation
+], deleteProduct);
+
+export default router;
